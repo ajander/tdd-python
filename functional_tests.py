3c1b1503afe5c8a1d49bbcb18915c3e8243b5d9a
ajander <adrienne.j.anderson@gmail.com>
2022-02-19 21:12:51 -0600

    functional test now checks can add a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,13 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # Notices page title says something about to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do_item'
+                )
 
         # Type first item
+        inputbox.send_keys('first item')
 
         # When hits enter, page updates and shows first item
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: first item' for row in rows)
+                )
 
         # Still has a text box inviting you to enter a to-do item. Adds a second one.
 
