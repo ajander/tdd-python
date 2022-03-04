1f147ff0126c47c2db5e209b56cd4f450f5a58f3
ajander <adrienne.j.anderson@gmail.com>
2022-03-03 19:27:14 -0600

    add helper fn to check row text

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Heard about a web app, go check out the url
@@ -34,10 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # When hits enter, page updates and shows first item
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: first item', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: first item')
 
         # Still has a text box inviting you to enter a to-do item. Adds a second one.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Page updates and shows both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: first item', [row.text for row in rows])
-        self.assertIn('2: second item', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: first item')
+        self.check_for_row_in_list_table('2: second item')
 
         # Site generates a url with her items, with explanatory text telling her that
 
